e087a3ead75d5d309c92694da8c886ce0a3e8b01
gh0stsniper <suryapoojaryx@protonmail.com>
2022-12-15 13:23:16 +0530

    associated links and user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_070427) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_075006) do
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
